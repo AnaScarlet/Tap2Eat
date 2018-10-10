b01fe80e1b39b35d68f0e7cfda8e70435c5a244e
hwheel2 <hwheel2@students.towson.edu>
2018-10-10 19:08:08 -0400

    Activities Created

---
+++ app/src/main/java/edu/towson/cosc431/wheeler/tap2eat/activity_home.kt
@@ -0,0 +1,12 @@
+package edu.towson.cosc431.wheeler.tap2eat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
