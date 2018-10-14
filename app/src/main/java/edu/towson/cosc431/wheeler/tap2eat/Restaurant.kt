0289f0270fe07b1ca05bd6a9a7151f592f9ba7d5
Anastasiya Lazarenko <laz1anastasiya@gmail.com>
2018-10-14 18:14:16 -0400

    Functioning home screen

---
+++ app/src/main/java/edu/towson/cosc431/wheeler/tap2eat/Restaurant.kt
@@ -0,0 +1,4 @@
+package edu.towson.cosc431.wheeler.tap2eat
+
+//TODO: be able to pass an image here
+data class Restaurant(val name:String, val address:String, val description:String, val tags:ArrayList<String>)
--- app/src/main/java/edu/towson/cosc431/wheeler/tap2eat/activity_home.kt
+++ app/src/main/java/edu/towson/cosc431/wheeler/tap2eat/activity_home.kt
@@ -1,12 +1,47 @@
 package edu.towson.cosc431.wheeler.tap2eat
 
+import android.content.ComponentName
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class activity_home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        populateView()
+
+        order_button.setOnClickListener{ launchMenu() }
+    }
+
+    private fun launchMenu() {
+        intent = Intent()
+        intent.component = ComponentName(this, activity_menu::class.java)
+        this.startActivity(intent)
+    }
+
+    private fun populateView() {
+        val restntTags = ArrayList<String>()
+        restntTags.add("Italian")
+        restntTags.add("Pasta")
+        restntTags.add("Spaghetti")
+        restntTags.add("Fettuccine")
+        restntTags.add("Linguine")
+        val restnt = Restaurant("Italian Spaghetti", "123 Great Drive, Towson, MD",
+                "The best Italian restaurant in town. Offers the best spaghetti withh meatballs.", restntTags)
+
+        name.text = restnt.name
+        address.text = restnt.address
+        description.text = restnt.description
+        var tags:String = ""
+        val rIter = restntTags.iterator()
+        while(rIter.hasNext()) {
+            tags += rIter.next()+ ", "
+        }
+        type_tags.text = tags
+
     }
 }
