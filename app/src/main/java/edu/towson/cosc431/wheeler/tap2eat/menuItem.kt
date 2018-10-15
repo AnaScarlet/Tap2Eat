228a8f1f46ffa84b09f8d075fae90358abe4ba67
hwheel2 <hwheel2@students.towson.edu>
2018-10-15 18:39:56 -0400

    menu item view partially created

---
+++ app/src/main/java/edu/towson/cosc431/wheeler/tap2eat/menuItem.kt
@@ -0,0 +1,7 @@
+package edu.towson.cosc431.wheeler.tap2eat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+data class menuItem(val name: String,
+                val description: String)
